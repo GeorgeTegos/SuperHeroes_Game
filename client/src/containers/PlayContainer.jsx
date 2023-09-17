2bc853e55270b28c0858cbe1aafb53af556fb109
George Tegos <george.tegos.22@gmail.com>
2023-09-17 22:36:27 +0100

    Battleground setup, header

--- client/src/containers/PlayContainer.jsx
+++ client/src/containers/PlayContainer.jsx
@@ -1,5 +1,38 @@
 import React, { useState } from "react";
 import { useOutletContext } from "react-router";
+import styled from "styled-components";
+
+const BattleGroundWrapper = styled.div`
+  margin-top: 1.5rem;
+  background-color: grey;
+  min-height: 80vh;
+`;
+
+const BattleGroundHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  padding: 1rem 0.7rem 1rem 0.7rem;
+  border: 1px solid red;
+`;
+
+const PlayerOne = styled.div`
+  display: flex;
+  width: 30%;
+  justify-content: space-around;
+  font-weight: bold;
+`;
+
+const Rounds = styled.div`
+  font-weight: bolder;
+`;
+
+const PlayerTwo = styled.div`
+  display: flex;
+  font-weight: bold;
+
+  width: 30%;
+  justify-content: space-around;
+`;
 
 function PlayContainer() {
   const { cardsData } = useOutletContext();
@@ -23,33 +56,45 @@ function PlayContainer() {
       setDeckTwo(newDeck);
     }
   };
-  console.log("Deck One: ", deckOne);
-  console.log("Deck Two: ", deckTwo);
 
   return (
-    <div>
-      {deckOne.length == 0 && (
-        <button
-          onClick={(e) => {
-            handleCardsSplitIntoDeck(e.target.value);
-          }}
-          value="1"
-        >
-          Create Deck One
-        </button>
-      )}
+    <>
+      <div>
+        {deckOne.length == 0 && (
+          <button
+            onClick={(e) => {
+              handleCardsSplitIntoDeck(e.target.value);
+            }}
+            value="1"
+          >
+            Create Deck One
+          </button>
+        )}
 
-      {deckTwo.length == 0 && (
-        <button
-          onClick={(e) => {
-            handleCardsSplitIntoDeck(e.target.value);
-          }}
-          value="2"
-        >
-          Create Deck Two
-        </button>
-      )}
-    </div>
+        {deckTwo.length == 0 && (
+          <button
+            onClick={(e) => {
+              handleCardsSplitIntoDeck(e.target.value);
+            }}
+            value="2"
+          >
+            Create Deck Two
+          </button>
+        )}
+      </div>
+      <BattleGroundWrapper>
+        <BattleGroundHeader>
+          <PlayerOne>
+            <div>Player One</div> <div>Cards: 10</div> <div>Your Round</div>
+          </PlayerOne>
+          <Rounds>Round X</Rounds>
+          <PlayerTwo>
+            <div>Enemy's Round</div>
+            <div>Cards: 10</div> <div>Player Two</div>
+          </PlayerTwo>
+        </BattleGroundHeader>
+      </BattleGroundWrapper>
+    </>
   );
 }
 
